c777f9d34daabf3049ea111013a347ffe6368fe8
kaydenOnXps <kayden.lim.mingjie@gmail.com>
2026-07-17 13:31:53 +0800

    added collation of 05/06 to 25/26 seasons

---
+++ src/feats.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def rollingForm():
+    pass
+
+def elo():
+    # start them off at a base value-> keep with szns?
+    pass
+
+def squadValue():
+    pass
+
+def h2h():
+    # take from prev szns -> past 5 h2h games? 
+    pass
--- src/load_data.py
+++ src/load_data.py
@@ -1,2 +1,11 @@
+import numpy as np
 import pandas as pd
 
+# feature to add(rolling form) -> add features 1 by 1 and compare effect 
+filename = "0506"
+df_raw = pd.read_csv(f"../data/{filename}.csv") # loads respective year csv into a dataframe (df)
+df_feats = pd.DataFrame()
+
+
+# TODO: rolling home/away integration
+# for the respective team, select by the team, slice last 6 games/view df[<home_team>][cur - 6 :cur]
